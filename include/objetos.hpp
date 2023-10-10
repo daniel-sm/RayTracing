56bfcbcbe8685d153b66f29648a811aff763f141
Daniel Santos <danieldsm.on@gmail.com>
2023-10-10 00:46:31 -0300

    Criando uma hierarquia de classes de fontes, onde podem haver diversos tipos fonte de luz, todas herdam os atributos comuns de Fonte

--- include/objetos.hpp
+++ include/objetos.hpp
@@ -30,4 +30,45 @@ public:
 	Ponto pontoIntersecao (double t) { return { ini + (t * dir) }; }
 }; // fim class Raio
 
+// Hierarquia de classes: Fonte
+class Fonte 
+{
+public:
+	Vetor intensidade; // intensidade da luz
+
+    virtual Vetor calcIntensidade(Vetor, Ponto, Vetor, Material) const = 0;
+};
+
+class Pontual : public Fonte
+{
+    Ponto posicao; // local da luz
+public:
+    Pontual (Vetor i, Ponto p) : posicao{p} { intensidade = i; }
+
+    Ponto getPosicao () { return posicao; }
+
+    Vetor calcIntensidade (Vetor n, Ponto p_int, Vetor dr, Material m) 
+    const override
+	{
+        // Vetor em direcao a fonte de luz  (unitario)
+        Vetor l = unitario(posicao - p_int);
+        // Vetor em direcao a origem do raio (unitario)
+        Vetor v = -1 * dr;
+        // Vetor em direcao ao raio refletido (unitario)
+        Vetor r = (2 * escalar(l, n) * n) - l;
+
+        // Fator de Difusao
+        double fatorDif = maior(0.0, escalar(l, n));
+        // Luz Difusa
+        Vetor Id = (m.kd * intensidade) * fatorDif; 
+
+        // Fator Especular
+        double fatorEsp = pow(maior(0.0, escalar(r, v)), m.brilho);
+        // Luz Especular 
+        Vetor Ie = (m.ke * intensidade) * fatorEsp; 
+
+        return (Id + Ie);
+	}
+};
+
 #endif
