d3bfda681d4cc665cf66f1941b25781f89977532
Daniel Santos <danieldsm.on@gmail.com>
2023-10-10 00:58:06 -0300

    Reorganizando a lógica da main, melhorando o cálculo da intensidade de luz em um ponto e caprichando nos comentários

--- main.cpp
+++ main.cpp
@@ -10,102 +10,192 @@ g++ main.cpp -o main.exe -I "C:\MinGW\include\SDL2" -lmingw32 -lSDL2main -lSDL2
 */
 
 #include <SDL.h>
-#include <cmath>
 #include "include/objetos.hpp"
 
-int main(int argc, char* argv[]) {
-// Ponto de visao da cena, origem dos raios
-	Ponto olho { 0, 0, 0 };
+int main(int argc, char** argv) 
+{
+	// Ponto de visao da cena, origem dos raios
+	Ponto eye { 0, 0, 0 };
 
-	// Informacoes da luz 
-	Ponto pFonte { 0, 5, 0 };
-	Vetor iFonte { 0.7, 0.7, 0.7 };
+	// Informacoes da janela ***************************************************
+	double wJanela = 60; // Largura da janela em CM
+	double hJanela = 60; // Altura da janela em CM
+	double dJanela = 30; // Distancia da janela em CM
+	Janela janela (wJanela, hJanela, dJanela); // Objeto da janela
 
-	// Fator do material da esfera sobre uma luz
-	Vetor kEsfera { 1.0, 0.0, 0.0 }; 
-	double mEsfera = 10; // fator espescular
+	// Informacoes do Canvas ***************************************************
+	int nCol = 500; // Numero de colunas da grade do canvas
+	int nLin = 500; // Numero de linhas da grade do canvas
 
-	double wJanela = 2; // Largura da janela
-	double hJanela = 2; // Altura da janela
-	double dJanela = 1; // Distancia da janela
+	// Informacoes da Esfera ***************************************************
+	double raioEsfera = 40; // Raio da esfera em CM
+	Ponto centroEsfera { 0, 0, -100 }; // Centro da esfera
+	// Propriedades de reflectividade da esfera
+	Vetor kaEsfera { 0.7, 0.2, 0.2 }; // Propr. ambiente do material da esfera 
+	Vetor kdEsfera = kaEsfera; // Propr. difusa do material da esfera 
+	Vetor keEsfera = kaEsfera; // Propr. especular do material da esfera 
+	double brilhoEsfera = 10; // Fator de brilho especular
+	// Material da esfera
+	Material materialEsfera { kaEsfera, kdEsfera, keEsfera, brilhoEsfera };
+	// Objeto da Esfera
+	Esfera esfera (centroEsfera, raioEsfera, materialEsfera);
+
+	// Informacoes do Plano do Chao ********************************************
+	Ponto pontoChao { 0, -raioEsfera, 0 }; // Ponto presente no plano do chao
+	Vetor normalChao { 0, 1, 0 }; // Vetor normal ao plano do chao
+	// Propriedades de reflectividade do plano do chao
+	Vetor kaChao { 0.2, 0.7, 0.2 }; // Propr. ambiente do material do chao 
+	Vetor kdChao = kaChao; // Propr. difusa do material do chao 
+	Vetor keChao { 0.0, 0.0, 0.0 }; // Propr. difusa do material do chao 
+	double brilhoChao = 1; // Fator de brilho espescular
+	// Material do plano do chao
+	Material materialChao { kaChao, kdChao, keChao, brilhoChao };
+	// Objeto do Plano do Chao
+	Plano chao (pontoChao, normalChao, materialChao);
+
+	// Informacoes do Plano do Fundo *******************************************
+	Ponto pontoFundo { 0, 0, -200 }; // Ponto presente no plano do fundo
+	Vetor normalFundo { 0, 0, 1 }; // Vetor normal ao plano do fundo
+	// Propriedades de reflectividade do plano de fundo
+	Vetor kaFundo { 0.3, 0.3, 0.7 }; // Propr. ambiente do material do fundo 
+	Vetor kdFundo = kaFundo; // Propr. difusa do material do fundo 
+	Vetor keFundo { 0.0, 0.0, 0.0 }; // Propr. difusa do material do fundo 
+	double brilhoFundo = 1; // Fator de brilho espescular
+	// Material do plano de fundo
+	Material materialFundo { kaFundo, kdFundo, keFundo, brilhoFundo };
+	// Objeto do Plano do Fundo
+	Plano fundo (pontoFundo, normalFundo, materialFundo);
 	
-	// Tamanho do raio da esfera 
-	double rEsfera = 1;
+	// Informacoes da Fonte Pontual ********************************************
+	Vetor intensePontual { 0.7, 0.7, 0.7 };
+	Ponto posicaoPontual { 0, 60, -30 };
+	// Objeto da Fonte Pontual de luz
+	Pontual pontual (intensePontual, posicaoPontual);
 
-	// Coordenadas do centro da esfera
-	Ponto cEsfera { 0, 0, -(dJanela + rEsfera) };
+    // Informacoes da luz Ambiente *********************************************
+    Vetor luzAmbiente { 0.3, 0.3, 0.3 };
 
-	Cor esfColor { 255, 0, 0 }; // Cor da esfera
-	Cor bgColor { 100, 100, 100 }; // Cor de fundo
+	// Lista de Objetos da Cena ************************************************
+	Lista<Objeto> cena;
+	// Adicionando os objetos na cena
+	cena.add(&esfera);
+	cena.add(&chao);
+	cena.add(&fundo);
 
-	int nCol = 600; // Numero de colunas da grade
-	int nLin = 600; // Numero de linhas da grade
+	// Lista de Fontes de luz **************************************************
+	Lista<Pontual> fontes;
+	// Adicionando as fontes na lista
+	fontes.add(&pontual);
 
-	// Matriz de cores
-	Cor** mCores = new Cor*[nLin];
+	// Matriz de cores *********************************************************
+	Cor** cores = new Cor*[nLin];
+	// Inicializando cada linha da matriz
+	for (int i = 0; i < nLin; ++i) { cores[i] = new Cor[nCol]; }
 
-	// Inicializando linhas da matriz
-	for (int i = 0; i < nLin; i++) {
-		mCores[i] = new Cor[nCol];
-	}
+	// Maior intensidade de cor ************************************************
+	// Usada para reprocessar as cores com base na maior
+	double maiorCor = 0.0; 
 
-	// Delta X e Y dos quadriculos da grade
-	double Dx = wJanela/nCol;
-	double Dy = hJanela/nLin;
+	// Delta X e Y dos quadrados da grade do canvas ****************************
+	double Dx = janela.getWidth()/nCol;
+	double Dy = janela.getHeight()/nLin;
 
-	for (int lin = 0; lin < nLin; lin++) {
+	// Loop de Ray Casting *****************************************************
+	// Lancando raios para cada quadrado na grade do canvas
+	//
+	// Percorre as linhas da grade do canvas
+	for (int lin = 0; lin < nLin; ++lin) 
+	{
 		// Coordenada Y do centro do quadriculo no frame
-		double y = hJanela/2 - Dy/2 - lin*Dy;
+		double y = (janela.getHeight()/2) - Dy/2 - lin*Dy;
 
-		for (int col = 0; col < nCol; col++) {
+		// Percorre as colunas da grade do canvas
+		for (int col = 0; col < nCol; ++col) 
+		{
 			// Coordenada X do centro do quadriculo no frame
-			double x = -(wJanela/2) + (Dx/2) + (col*Dx);
+			double x = -(janela.getWidth()/2) + (Dx/2) + (col*Dx);
 
-			Raio R = Raio (olho, { x, y, -dJanela });
-			Esfera E = Esfera (cEsfera, rEsfera);
+			// Gerando raio lancado pela janela
+			Raio raio = Raio (eye, { x, y, -janela.getDistance() });
 
-			double t = E.intersecao(R);
+			// Armazena o objeto intersectado mais proximo 
+			Objeto* atingido = nullptr; 
+			// Armazena o valor de t que intersecta o objeto mais proximo
+			double t_int = -1;
 
-			if (t > 0) {
-				Ponto pInt = R.P(t);
+			//Percorrendo os objetos da cena
+			for (auto obj : cena)
+			{
+				// Guardando o retorno da intersecao para comparar com 't_int'
+				double t = obj->intersecao(raio);
 
-				Vetor l = unitario(pFonte - pInt);
-				Vetor n = (pInt - cEsfera) / rEsfera;
-				Vetor v = R.direcao() * -1;
-				Vetor r = (n * 2 * escalar(l, n)) - l;
+				// Computando o menor valor de t_int
+				if (t > 0) 
+				{
+					if (t_int > 0) 
+					{ 
+						if (t < t_int) 
+						{ 
+							t_int = t; 
+							atingido = obj; 
+						}
+					} 
+					else { t_int = t; atingido = obj; }
+				}
+			}
 
-				Vetor luz = (kEsfera * iFonte);
+			// Verificando de atingiu algum objeto
+			if (t_int > 0) 
+			{
+                Ponto p_int = raio.pontoIntersecao(t_int);
 
-				double fatorDif = escalar(l, n);
+				// Vetor de intensidade da luz no ponto intersectado
+				Vetor I { 0.0, 0.0, 0.0 };
+				// Adicionando a luz ambiente
+				I = I + (luzAmbiente * atingido->material.ka);
 
-				Vetor Id { 0.0, 0.0, 0.0 };
-				if (fatorDif >= 0) Id = luz * fatorDif;
+				// Percorrendo as fontes de luz da cena
+				for (auto fonte : fontes)
+				{
+					Vetor normal = atingido->obterNormal(p_int);
 
-				double fatorEsp = escalar(v, r);
+					// Armazena a intensidade no ponto para a fonte atual
+					Vetor aux = fonte->calcIntensidade(
+						normal,
+						p_int,
+						raio.direcao(),
+						atingido->material
+					);
 
-				Vetor Ie { 0.0, 0.0, 0.0 };
-				if (fatorEsp >= 0) {
-					fatorEsp = pow(fatorEsp, mEsfera);
-					Ie = luz * fatorEsp;
+					// Soma a intensidade de cada fonte
+					I = I + aux;
 				}
 
-				Vetor I = Id + Ie;
+				// Computando a maior intensidade de cor
+				if (I.a > maiorCor) maiorCor = I.a;
+				if (I.b > maiorCor) maiorCor = I.b;
+				if (I.c > maiorCor) maiorCor = I.c;
 
 				Cor cor { 255, 255, 255 };
 
+				// Arredondar para valor inteiro
 				cor.r = round(cor.r * I.a);
 				cor.g = round(cor.g * I.b);
 				cor.b = round(cor.b * I.c);
 
-				if (cor.r > 255) cor.r = 255;
-				if (cor.g > 255) cor.g = 255;
-				if (cor.b > 255) cor.b = 255;
-
-				mCores[lin][col] = cor;
+				cores[lin][col] = cor;
+			} else { 
+				cores[lin][col] = { 0, 0, 0 };
 			}
-			else { 
-				mCores[lin][col] = bgColor;
-}
+		}
+	}
+
+	// Reprocessar as cores com base na maior intensidade de cor
+	for (int l = 0; l < nLin; ++l) {
+		for (int c = 0; c < nCol; ++c) {
+			cores[l][c].r = round(cores[l][c].r / maiorCor);
+			cores[l][c].g = round(cores[l][c].g / maiorCor);
+			cores[l][c].b = round(cores[l][c].b / maiorCor);
 		}
 	}
 
@@ -163,18 +253,18 @@ int main(int argc, char* argv[]) {
 		// Limpando o renderer
 		SDL_RenderClear(renderer);
 		
-		for (int i = 0; i < nLin; i++) {
-			for (int j = 0; j < nCol; j++) {
+		for (int y = 0; y < nLin; ++y) {
+			for (int x = 0; x < nCol; ++x) {
 				// Designando a cor que vai pintar
 				SDL_SetRenderDrawColor(
 					renderer, 
-					mCores[i][j].r, 
-					mCores[i][j].g, 
-					mCores[i][j].b, 
+					cores[y][x].r, 
+					cores[y][x].g, 
+					cores[y][x].b, 
 					SDL_ALPHA_OPAQUE
 				);
 				// Pintando o pixel
-				SDL_RenderDrawPoint(renderer, j, i);
+				SDL_RenderDrawPoint(renderer, x, y);
 			}
 		}
 		// Atualizando a janela com o renderer
@@ -185,11 +275,9 @@ int main(int argc, char* argv[]) {
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
-	// Desalocando a matriz de cores
-	for (int i = 0; i < nLin; i++) {
-		delete mCores[i];
-	}
-	delete[] mCores;
+	// Desalocando cada linha da matriz de cores
+	for (int i = 0; i < nLin; ++i) { delete cores[i]; }
+	delete[] cores; // Desaloca a matriz por completo
 
 	return 0;
 }
